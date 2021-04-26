8ddcc1e79675ad21f23e391ce6f0257d325cdca2
Denny Abbas Zain <abbasdenny24@gmail.com>
2021-04-26 13:46:39 +0700

    v1.2 refactoring dan menambahkan fitur async await

--- js/app.js
+++ js/app.js
@@ -1,124 +1,96 @@
 // API
-const proxy = 'https://cors-anywhere.herokuapp.com/'
-const ID = 'https://covid19.mathdro.id/api/countries/indonesia/confirmed'
-const PROV = 'https://api.kawalcorona.com/indonesia/provinsi'
-const NEGARA = 'https://api.kawalcorona.com'
-// const POSITIF = 'https://api.kawalcorona.com/positif'
-// const SEMBUH = 'https://api.kawalcorona.com/sembuh'
-// const MENINGGAL = 'https://api.kawalcorona.com/meninggal'
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const ID = 'https://covid19.mathdro.id/api/countries/indonesia/confirmed';
+const PROV = 'https://api.kawalcorona.com/indonesia/provinsi';
+const NEGARA = 'https://api.kawalcorona.com';
 
+//DOM HTML
+const rowsID = document.querySelector('.rowsID');
+const rowsProv = document.querySelector('.list-provinsi');
+const rowsNegara = document.querySelector('.list-negara');
+const waktuD = document.querySelector('.waktu');
+const idConfirmed = document.getElementById('confirmed');
+const idRecovered = document.getElementById('recovered');
+const idDeath = document.getElementById('death');
 
-// INdonesia DOM
-const rowsID = document.querySelector('.rowsID')
-const rowsProv = document.querySelector('.list-provinsi')
-const rowsNegara = document.querySelector('.list-negara')
-const waktuD = document.querySelector('.waktu')
+// Main Function
 
+const main = async () => {
+    const resW = await indo();
+    const resIndo = await sDataIndo();
+    const resDunia = await sDataDunia();
+    console.log(resW);
+    console.log(resIndo);
+    console.log(resDunia);
+    let i = '';
+    let no = 1;
 
-getIdData()
+    resIndo.forEach((d) => {
+        const pD = d.attributes;
+        i += tagProvinsi(pD, no++);
+        rowsProv.innerHTML = i;
+    });
 
-getProvData()
+    let o = '';
+    let noN = 1;
 
+    resDunia.forEach((d) => {
+        const nD = d.attributes;
+        o += tagNegara(nD, noN++);
+        rowsNegara.innerHTML = o;
+    });
 
-function getIdData() {
-    return fetch(`${proxy}${ID}`)
-        .then(response => response.json())
-        .then(response => {
-            response.forEach(id => {
-                rowsID.innerHTML = tagID(id)
+    resW.forEach((d) => {
+        const dt = d;
+        idConfirmed.innerText = d.confirmed;
+        idRecovered.innerText = d.recovered;
+        idDeath.innerText = d.deaths;
+        waktuD.innerHTML = `<h6>Update Terakhir : <br>${new Date(
+            dt.lastUpdate
+        )}</h6>`;
+    });
+};
 
-            })
-        })
-}
+// Getting API Using Async await
 
+const sDataDunia = () => {
+    return fetch(NEGARA)
+        .then((res) => res.json())
+        .then((res) => res);
+};
 
+const sDataIndo = () => {
+    return fetch(PROV)
+        .then((res) => res.json())
+        .then((res) => res);
+};
 
+const indo = () => {
+    return fetch(ID)
+        .then((res) => res.json())
+        .then((res) => res);
+};
 
+main();
 
-function getProvData() {
-    return fetch(`${proxy}${PROV}`)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            let i = ''
-            let no = 1;
-            response.forEach(p => {
-                const pD = p.attributes;
-                i += tagProvinsi(pD, no++)
-                rowsProv.innerHTML = i
+// HTML with template literal
 
-            })
-        })
-}
-
-
-getNegaraData()
-
-
-function getNegaraData() {
-    return fetch(`${proxy}${NEGARA}`)
-        .then(response => response.json())
-        .then(response => {
-            let o = ''
-            let no = 1;
-            response.forEach(n => {
-                const nD = n.attributes;
-                o += tagNegara(nD, no++)
-                rowsNegara.innerHTML = o
-                const or = nD.Last_Update
-                const waktu = new Date(or)
-                waktuD.innerHTML = `<h6>Update Terakhir : <br>${waktu.toUTCString()}</h6>`
-
-            })
-        })
-}
-
-
-
-function tagID(id) {
-    return ` <div class="col-md-4 mt-5 ">
-  
-                <h4 class="">Total Kasus Se-Indonesia</h4>
-                <div class="box box-1 shadow text-light">
-                    <img class="conge" src="img/sad.png" alt="">
-                    <span >${id.confirmed}</span>
-                    <h5 class="text-dark">Positif</h5>
-                </div>
-                <div class="box box-2 shadow text-light">
-                    <img class = "conge"
-                    src ="img/happy.png"
-                    alt = "" >
-                    <span >${id.recovered}</span>
-                    <h5 class="text-dark">Sembuh</h5>
-                </div>
-                <div class="box box-3 shadow text-light">
-                    <img class="conge"
-                    src="img/cry.png"
-                    alt="" >
-                    <span >${id.deaths}</span>
-                    <h5 class="text-dark">Meninggal</h5>
-                </div>
-            </div>`
-}
-
-function tagProvinsi(pD, no) {
+function tagProvinsi(pD, noN) {
     return `                <tr>
-                                <th scope="row">${no}</th>
+                                <th scope="row">${noN}</th>
                                 <td>${pD.Provinsi}</td>
                                 <td>${pD.Kasus_Posi}</td>
                                 <td> ${pD.Kasus_Semb} </td>
                                 <td> ${pD.Kasus_Meni} </td>
-                            </tr>`
+                            </tr>`;
 }
 
-
 function tagNegara(nD, no) {
-    return ` <tr>
+    return `             <tr>
                                 <th scope="row">${no}</th>
                                 <td>${nD.Country_Region}</td>
                                 <td>${nD.Confirmed}</td>
                                 <td> ${nD.Recovered} </td>
                                 <td> ${nD.Deaths} </td>
-                            </tr>`
-
+                            </tr>`;
 }
